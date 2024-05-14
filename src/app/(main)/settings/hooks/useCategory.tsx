bb36ddd8e0d0392ba0323bbe82bf3c48dd49b4dd
ryiann <me@ryana.cn>
2024-05-14 23:53:26 +0800

    feat: 删除会话设置多余选项

--- src/app/(main)/settings/hooks/useCategory.tsx
+++ src/app/(main)/settings/hooks/useCategory.tsx
@@ -1,9 +1,7 @@
 import { Icon } from '@lobehub/ui';
-import { Tag } from 'antd';
-import { Bot, Brain, Cloudy, Info, Mic2, Settings2 } from 'lucide-react';
+import { Bot, Brain, Mic2, Settings2 } from 'lucide-react';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Flexbox } from 'react-layout-kit';
 
 import type { MenuProps } from '@/components/Menu';
 import { SettingsTabs } from '@/store/global/initialState';
@@ -21,18 +19,6 @@ export const useCategory = () => {
           key: SettingsTabs.Common,
           label: t('tab.common'),
         },
-        enableWebrtc && {
-          icon: <Icon icon={Cloudy} />,
-          key: SettingsTabs.Sync,
-          label: (
-            <Flexbox align={'center'} gap={8} horizontal>
-              {t('tab.sync')}
-              <Tag bordered={false} color={'warning'}>
-                {t('tab.experiment')}
-              </Tag>
-            </Flexbox>
-          ),
-        },
         showLLM && {
           icon: <Icon icon={Brain} />,
           key: SettingsTabs.LLM,
@@ -43,11 +29,6 @@ export const useCategory = () => {
           icon: <Icon icon={Bot} />,
           key: SettingsTabs.Agent,
           label: t('tab.agent'),
-        },
-        {
-          icon: <Icon icon={Info} />,
-          key: SettingsTabs.About,
-          label: t('tab.about'),
         },
       ].filter(Boolean) as MenuProps['items'],
     [t, enableWebrtc, showLLM],
