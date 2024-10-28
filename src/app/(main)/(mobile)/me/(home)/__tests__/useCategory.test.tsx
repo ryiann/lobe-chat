e43444e040a690fe4388d52b45660c05fb2a727a
ryiann <me@ryana.cn>
2024-10-29 03:20:39 +0800

    修改校验

--- src/app/(main)/(mobile)/me/(home)/__tests__/useCategory.test.tsx
+++ src/app/(main)/(mobile)/me/(home)/__tests__/useCategory.test.tsx
@@ -55,9 +55,6 @@ describe('useCategory', () => {
       expect(items.some((item) => item.key === 'profile')).toBe(false);
       expect(items.some((item) => item.key === 'setting')).toBe(true);
       expect(items.some((item) => item.key === 'data')).toBe(true);
-      expect(items.some((item) => item.key === 'docs')).toBe(true);
-      expect(items.some((item) => item.key === 'feedback')).toBe(true);
-      expect(items.some((item) => item.key === 'discord')).toBe(true);
     });
   });
 
@@ -74,9 +71,6 @@ describe('useCategory', () => {
       expect(items.some((item) => item.key === 'profile')).toBe(true);
       expect(items.some((item) => item.key === 'setting')).toBe(true);
       expect(items.some((item) => item.key === 'data')).toBe(true);
-      expect(items.some((item) => item.key === 'docs')).toBe(true);
-      expect(items.some((item) => item.key === 'feedback')).toBe(true);
-      expect(items.some((item) => item.key === 'discord')).toBe(true);
     });
   });
 
@@ -93,9 +87,6 @@ describe('useCategory', () => {
       expect(items.some((item) => item.key === 'profile')).toBe(false);
       expect(items.some((item) => item.key === 'setting')).toBe(true);
       expect(items.some((item) => item.key === 'data')).toBe(true);
-      expect(items.some((item) => item.key === 'docs')).toBe(true);
-      expect(items.some((item) => item.key === 'feedback')).toBe(true);
-      expect(items.some((item) => item.key === 'discord')).toBe(true);
       expect(items.some((item) => item.key === 'nextauthSignout')).toBe(true);
     });
   });
@@ -112,9 +103,6 @@ describe('useCategory', () => {
       expect(items.some((item) => item.key === 'profile')).toBe(false);
       expect(items.some((item) => item.key === 'setting')).toBe(false);
       expect(items.some((item) => item.key === 'data')).toBe(false);
-      expect(items.some((item) => item.key === 'docs')).toBe(true);
-      expect(items.some((item) => item.key === 'feedback')).toBe(true);
-      expect(items.some((item) => item.key === 'discord')).toBe(true);
       expect(items.some((item) => item.key === 'nextauthSignout')).toBe(false);
     });
   });
