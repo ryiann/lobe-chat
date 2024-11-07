a0e3a9dd0a1f3a6358793b0fb49c43e645f9d964
ryiann <me@ryana.cn>
2024-11-07 23:33:37 +0800

    去除发现页面分享按钮

--- src/app/(main)/discover/(detail)/assistant/[slug]/features/Actions.tsx
+++ src/app/(main)/discover/(detail)/assistant/[slug]/features/Actions.tsx
@@ -2,12 +2,9 @@
 
 import { memo } from 'react';
 import { Flexbox, FlexboxProps } from 'react-layout-kit';
-import urlJoin from 'url-join';
 
-import { OFFICIAL_URL } from '@/const/url';
 import { DiscoverAssistantItem } from '@/types/discover';
 
-import ShareButton from '../../../features/ShareButton';
 import AddAgent from './AddAgent';
 
 interface AssistantActionProps extends FlexboxProps {
@@ -15,19 +12,10 @@ interface AssistantActionProps extends FlexboxProps {
   identifier: string;
 }
 
-const AssistantAction = memo<AssistantActionProps>(({ identifier, data }) => {
+const AssistantAction = memo<AssistantActionProps>(({ data }) => {
   return (
     <Flexbox align={'center'} gap={8} horizontal>
       <AddAgent data={data} />
-      <ShareButton
-        meta={{
-          avatar: data.meta.avatar,
-          desc: data.meta.description,
-          hashtags: data.meta.tags,
-          title: data.meta.title,
-          url: urlJoin(OFFICIAL_URL, '/discover/assistant', identifier),
-        }}
-      />
     </Flexbox>
   );
 });
