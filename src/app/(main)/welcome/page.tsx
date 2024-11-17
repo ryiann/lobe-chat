4c5980f9c3892bd7f573d17588d51060bb714ffe
ryiann <me@ryana.cn>
2024-11-17 21:34:32 +0800

    去除欢迎页Logo

--- src/app/(main)/welcome/page.tsx
+++ src/app/(main)/welcome/page.tsx
@@ -1,4 +1,3 @@
-import { WelcomeLogo } from '@/components/Branding';
 import StructuredData from '@/components/StructuredData';
 import { BRANDING_NAME } from '@/const/branding';
 import { ldModule } from '@/server/ld';
@@ -30,7 +29,6 @@ const Page = async () => {
   return (
     <>
       <StructuredData ld={ld} />
-      <WelcomeLogo mobile={mobile} />
       <Hero />
       <Actions mobile={mobile} />
     </>
