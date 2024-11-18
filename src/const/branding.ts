d78e56b7518cd3d06858a089b8f8ad30082728b0
ryiann <me@ryana.cn>
2024-11-19 01:06:17 +0800

    修改Logo

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -7,7 +7,7 @@ export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 export const BRANDING_NAME = 'LobeChat';
 export const NEW_BRANDING_NAME = 'FunChat';
 
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_LOGO_URL = 'https://cdn.ryana.cn/funchat/app.png';
 
 export const ORG_NAME = 'LobeHub';
 export const NEW_ORG_NAME = 'FunChat';
