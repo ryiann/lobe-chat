01833a1d1689c02c5da062698ecc0e0fa4a1e6ef
ryan <me@ryana.cn>
2025-04-18 21:27:56 +0800

    修改移动端App名称

--- src/app/[variants]/(main)/chat/@session/_layout/Mobile/SessionHeader.tsx
+++ src/app/[variants]/(main)/chat/@session/_layout/Mobile/SessionHeader.tsx
@@ -5,11 +5,9 @@ import { MobileNavBar } from '@lobehub/ui/mobile';
 import { MessageSquarePlus } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { memo } from 'react';
-import { Flexbox } from 'react-layout-kit';
+import { Center, Flexbox } from 'react-layout-kit';
 
-import { ProductLogo } from '@/components/Branding';
 import { MOBILE_HEADER_ICON_SIZE } from '@/const/layoutTokens';
-import SyncStatusInspector from '@/features/SyncStatusInspector';
 import UserAvatar from '@/features/User/UserAvatar';
 import { featureFlagsSelectors, useServerConfigStore } from '@/store/serverConfig';
 import { useSessionStore } from '@/store/session';
@@ -18,15 +16,16 @@ import { mobileHeaderSticky } from '@/styles/mobileHeader';
 const Header = memo(() => {
   const [createSession] = useSessionStore((s) => [s.createSession]);
   const router = useRouter();
-  const { enableWebrtc, showCreateSession } = useServerConfigStore(featureFlagsSelectors);
+  const { showCreateSession } = useServerConfigStore(featureFlagsSelectors);
 
   return (
     <MobileNavBar
       left={
         <Flexbox align={'center'} gap={8} horizontal style={{ marginLeft: 8 }}>
           <UserAvatar onClick={() => router.push('/me')} size={32} />
-          <ProductLogo type={'text'} />
-          {enableWebrtc && <SyncStatusInspector placement={'bottom'} />}
+          <Center style={{ color: 'black', fontSize: '1.3rem', fontWeight: 'bold' }}>
+            {'FunChat'}
+          </Center>
         </Flexbox>
       }
       right={
