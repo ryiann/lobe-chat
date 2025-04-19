a7d21d153aa5ede7f143127f49f79261eebca504
ryan <me@ryana.cn>
2025-04-19 13:16:33 +0800

    修改数据统计分享

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -6,7 +6,9 @@ export const LOBE_CHAT_CLOUD = 'FunChat Cloud';
 
 export const BRANDING_NAME = 'FunChat';
 export const BRANDING_LOGO_URL = '';
-export const CUSTOM_BRANDING_LOGO_URL = 'https://cdn.ryana.cn/funchat/app.png';
+
+export const CUSTOM_BRANDING_LOGO_URL = 'https://cdn.funtui.com/logo.png';
+export const CUSTOM_BRANDING_APP_LOGO_URL = 'https://cdn.ryana.cn/funchat/app.png';
 
 export const ORG_NAME = 'FunChat';
 
