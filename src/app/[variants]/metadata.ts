7974d2538b05c1c6546feebd7c498970045c1967
ryan <me@ryana.cn>
2025-06-09 00:10:00 +0800

    修改导航栏图标

--- src/app/[variants]/metadata.ts
+++ src/app/[variants]/metadata.ts
@@ -1,7 +1,7 @@
-import { BRANDING_LOGO_URL, BRANDING_NAME, ORG_NAME } from '@/const/branding';
+import { BRANDING_NAME, ORG_NAME } from '@/const/branding';
 import { DEFAULT_LANG } from '@/const/locale';
 import { OFFICIAL_URL, OG_URL } from '@/const/url';
-import { isCustomBranding, isCustomORG } from '@/const/version';
+import { isCustomORG } from '@/const/version';
 import { appEnv } from '@/envs/app';
 import { translation } from '@/server/translation';
 import { DynamicLayoutProps } from '@/types/next';
@@ -25,13 +25,11 @@ export const generateMetadata = async (props: DynamicLayoutProps) => {
       title: BRANDING_NAME,
     },
     description: t('chat.description', { appName: BRANDING_NAME }),
-    icons: isCustomBranding
-      ? BRANDING_LOGO_URL
-      : {
-          apple: '/apple-touch-icon.png?v=1',
-          icon: '/favicon.ico?v=1',
-          shortcut: '/favicon-32x32.ico?v=1',
-        },
+    icons: {
+      apple: 'https://cdn.funtui.com/apple-touch-icon.png?v=1',
+      icon: 'https://cdn.funtui.com/favicon.ico?v=1',
+      shortcut: 'https://cdn.funtui.com/favicon-32x32.ico?v=1',
+    },
     manifest: noManifest ? undefined : '/manifest.json',
     metadataBase: new URL(OFFICIAL_URL),
     openGraph: {
